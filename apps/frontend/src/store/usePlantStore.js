87047503ea3eb48bbe94ccf94b12392845331cab
Taida <taida.carmona@xeria.es>
2026-07-24 15:12:09 +0200

    Cambio url fetchplants

--- apps/frontend/src/store/usePlantStore.js
+++ apps/frontend/src/store/usePlantStore.js
@@ -1,4 +1,4 @@
-import {create} from 'zustand'
+import { create } from 'zustand'
 // import initialPlants from '../data/plants.json'
 
 //Hook personalizado
@@ -6,38 +6,38 @@ export const usePlantStore = create((set) => ({
     plants: [],
     loading: false,
 
-    fetchPlants: async () =>{
-        set({loading: true})
-        try{
-            const res = await fetch('http://localhost:3000/plants')
+    fetchPlants: async () => {
+        set({ loading: true })
+        try {
+            const res = await fetch('https://plants-app-backend.onrender.com/plants')
             if (!res.ok) throw new Error('Error al conectar a la API')
             const data = await res.json()
-            set({plants: data, loading: false})
-        }catch (error){
+            set({ plants: data, loading: false })
+        } catch (error) {
             console.log(error)
-            set({loading: false})
+            set({ loading: false })
         }
     },
 
     waterPlant: (plantId) => set((state) => ({
         plants: state.plants.map((plant) =>
-        plant.id === plantId ? {...plant, watering: {...plant.watering, waterRecord: [...plant.watering.waterRecord, new Date().toISOString().split('T')[0]]}} : plant)
+            plant.id === plantId ? { ...plant, watering: { ...plant.watering, waterRecord: [...plant.watering.waterRecord, new Date().toISOString().split('T')[0]] } } : plant)
     })),
 
     fertilizePlant: (plantId) => set((state) => ({
         plants: state.plants.map((plant) =>
-        plant.id === plantId ? {...plant, fertilization: {...plant.fertilization, fertilizerRecord: [...plant.fertilization.fertilizerRecord, new Date().toISOString().split('T')[0]]}} : plant)
+            plant.id === plantId ? { ...plant, fertilization: { ...plant.fertilization, fertilizerRecord: [...plant.fertilization.fertilizerRecord, new Date().toISOString().split('T')[0]] } } : plant)
     })),
 
-    treatPlant: (plantId) => set((state) =>({
-        plants: state.plants.map((plant) => 
-        plant.id === plantId ? {...plant, treatment: {...plant.treatment, treatmentRecord: [...plant.treatment.treatmentRecord, new Date().toISOString().split('T')[0]]}} : plant
+    treatPlant: (plantId) => set((state) => ({
+        plants: state.plants.map((plant) =>
+            plant.id === plantId ? { ...plant, treatment: { ...plant.treatment, treatmentRecord: [...plant.treatment.treatmentRecord, new Date().toISOString().split('T')[0]] } } : plant
         )
     })),
 
     updatePlant: async (updatedPlant) => {
-        set({loading: true, error: null})
-        try{
+        set({ loading: true, error: null })
+        try {
             const res = await fetch(`http://localhost:3000/plants/${updatedPlant.id}`, {
                 method: 'PATCH',
                 headers: {
@@ -45,23 +45,23 @@ export const usePlantStore = create((set) => ({
                 },
                 body: JSON.stringify(updatedPlant)
             })
-            
-        if(!res.ok) throw new Error('Error en la petición')
-                
-        const result = await res.json()
 
-        set((state) => ({
-            plants: state.plants.map(p => p.id === result.id ? result : p),
-            loading:false
-    }))
-        }catch(error){
-            set({error: error.message, loading: false})
+            if (!res.ok) throw new Error('Error en la petición')
+
+            const result = await res.json()
+
+            set((state) => ({
+                plants: state.plants.map(p => p.id === result.id ? result : p),
+                loading: false
+            }))
+        } catch (error) {
+            set({ error: error.message, loading: false })
         }
     },
 
     deleteComment: (plantId, commentId) => set((state) => ({
         plants: state.plants.map((plant) => {
-            if (plant.id===plantId){
+            if (plant.id === plantId) {
                 return {
                     ...plant,
                     comments: plant.comments.filter(c => c.id !== commentId)
@@ -72,45 +72,46 @@ export const usePlantStore = create((set) => ({
         )
     })),
 
-    deletePlant: async (plant) =>{
-        set({error: null, loading: true})
-        try{
+    deletePlant: async (plant) => {
+        set({ error: null, loading: true })
+        try {
             const res = await fetch(`http://localhost:3000/plants/${plant.id}`, {
-            method: 'DELETE'
+                method: 'DELETE'
             })
 
-            if(!res.ok) throw new Error('Error en la petición')
-            
+            if (!res.ok) throw new Error('Error en la petición')
+
             set((state) => ({
                 plants: state.plants.filter(p => p.id !== plant.id),
                 loading: false
             }))
-            
-        }catch(error){
-            set({error:error.message, loading: false})
+
+        } catch (error) {
+            set({ error: error.message, loading: false })
         }
     },
 
     addPlant: async (newPlant) => {
-        set({loading: true, error: null})
-        try{
+        set({ loading: true, error: null })
+        try {
             const res = await fetch('http://localhost:3000/plants', {
                 method: 'POST',
                 headers: {
-                    'Content-Type': 'application/json' },
+                    'Content-Type': 'application/json'
+                },
                 body: JSON.stringify(newPlant)
             })
 
-            if(!res.ok) throw new Error('Error en la petición')
-            
+            if (!res.ok) throw new Error('Error en la petición')
+
             const result = await res.json()
             set((state) => ({
                 plants: [...state.plants, result],
                 loading: false
             }))
 
-        }catch(error){
-            set({error: error.message, loading: false})
+        } catch (error) {
+            set({ error: error.message, loading: false })
         }
     }
 }))
